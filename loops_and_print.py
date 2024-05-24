4cb2e77eb61e783ea42cd3cb332d50d591adaee6
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-05-23 22:03:38 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,13 @@
 def enumerate_list(list):
+    nlist=[]
+    for index, elementos in enumerate(list):
+        if elemento:
+            nlist.append(f"{index}. {elemento}")
     return list
 
 
 def enumerate_backwards(list):
+    nlist=[]
+    for index, elemento in enumerate(list[::-1]):
+        nlist.append(f"{index}.{elemento[::-1]}")
     return list
